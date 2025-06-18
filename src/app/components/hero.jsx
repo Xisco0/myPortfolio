01bc73272284ed299f2b01c209d89428ef3594e8
Bamirin Francis <francisbamirin45@gmail.com>
2025-06-18 14:32:32 +0100

    new commit

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -11,8 +11,8 @@ return (
         A multi-stack developer building reliable, scalable web and desktop applications using modern tech like <span className="text-white font-medium">Next.js</span>, <span className="text-white font-medium">PHP</span>, <span className="text-white font-medium">VB.NET</span>, and <span className="text-white font-medium">MySQL</span>.
       </p>
       <div className="flex gap-4">
-        <a href="/document/bamirin-francis-cv.pdf" download="/document/bamirin-francis-cv.pdf" className="bg-white text-black px-5 py-2 rounded hover:bg-gray-200 transition">Download CV</a>
-        <a href="/document/bamirin-francis-cv.pdf" target="_blank" rel="noopener noreferrer" className="border border-white px-5 py-2 rounded text-white hover:bg-white hover:text-black transition">Print CV</a>
+        <a href="/document/Bamirin-Francis-Cv.pdf" download="/document/Bamirin-Francis-Cv.pdf" className="bg-white text-black px-5 py-2 rounded hover:bg-gray-200 transition">Download CV</a>
+        <a href="/document/Bamirin-Francis-Cv.pdf" target="_blank" rel="noopener noreferrer" className="border border-white px-5 py-2 rounded text-white hover:bg-white hover:text-black transition">Print CV</a>
       </div>
     </div>
     <div className="w-64 h-64 rounded-full overflow-hidden border-4 border-[#00FFE0] shadow-lg" data-aos="fade-right" data-aos-duration="2000">
